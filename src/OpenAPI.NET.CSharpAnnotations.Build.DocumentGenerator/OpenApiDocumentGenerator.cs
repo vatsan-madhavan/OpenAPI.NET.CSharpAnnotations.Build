c542b9099f4ca1615674988fd0a270e5ec2e4c0d
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2021-05-23 17:26:30 -0700

    Create OpenAPI.NET.CSharpAnnotations.Build.DocumentGenerator project

---
+++ src/OpenAPI.NET.CSharpAnnotations.Build.DocumentGenerator/OpenApiDocumentGenerator.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Vatsan Madhavan. All rights reserved.
+
+namespace OpenAPI.NET.CSharpAnnotations.Build
+{
+    /// <summary>
+    /// Generates Open API Document.
+    /// </summary>
+    public class OpenApiDocumentGenerator
+    {
+    }
+}
